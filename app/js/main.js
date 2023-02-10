c9f87f7e5978a887119310da3a33fe80fc115a73
oksanka1592 <ksuxa15lz@gmail.com>
2023-02-10 17:06:58 +0300

    added adaptiv header

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,14 @@
 $ (function(){
 
+  $('.header__burger').on('click', function (){
+    $('.header__burger,.header__menu').toggleClass('active');
+    $('body').toggleClass('lock');
+  });
+
+  $('.user-nuv__menu').on('click', function () {
+    $('.user-nuv__menu,.user-nuv__search,.user-nuv__link ').toggleClass('active');
+  });
+
   $('.slider__items').slick({
     dots: true,
     arrows: false,
@@ -8,4 +17,5 @@ $ (function(){
 
   var mixer = mixitup('.product__items');
   var mixer = mixitup('.desing__items');
+
 });
